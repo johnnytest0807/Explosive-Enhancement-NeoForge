d33beeba3ae03032c8577e78d6f3480666f2a80b
johnnytest0807 <70382764+johnnytest0807@users.noreply.github.com>
2026-02-15 13:26:46 +0800

    Update BlastWaveParticle.java

--- src/main/java/com/euphony/explosive_enhancement/particles/normal/BlastWaveParticle.java
+++ src/main/java/com/euphony/explosive_enhancement/particles/normal/BlastWaveParticle.java
@@ -31,9 +31,9 @@ public class BlastWaveParticle extends AbstractExplosiveParticle {
     @Override
     public void extract(QuadParticleRenderState submittable, Camera camera, float tickProgress) {
         Quaternionf quaternionf = new Quaternionf();
-        quaternionf.rotationX(); // rotate 90 degrees to be horizontal
-        this.extractRotatedQuad(submittable, camera, quaternionf, tickProgress);
-        quaternionf.rotateYXZ(HUNDRED_EIGHTY_DEGREES, 0, 0); // flip upside down to be seen from beneath
+        //quaternionf.rotationX(0); // rotate 90 degrees to be horizontal
+        //this.extractRotatedQuad(submittable, camera, quaternionf, tickProgress);
+        //quaternionf.rotateYXZ(HUNDRED_EIGHTY_DEGREES, 0, 0); // flip upside down to be seen from beneath
         this.extractRotatedQuad(submittable, camera, quaternionf, tickProgress);
     }
 
